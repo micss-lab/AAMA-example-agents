36116132407fbcffd75274a006cf883fd1c7f985
dogukan <dogukan.altay@student.uantwerpen.be>
2023-11-14 17:22:25 +0100

    Updated robot messages

--- src/main/java/RobotMsgs/IMU.java
+++ src/main/java/RobotMsgs/IMU.java
@@ -3,7 +3,7 @@ package RobotMsgs;
 public class IMU {
 
     public Header header = new Header();
-    public Orientation orientation = new Orientation();
+    public QuaternionOrientation orientation = new QuaternionOrientation();
     public Position angular_velocity = new Position();
     public Position linear_acceleration = new Position();
 
